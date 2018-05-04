d8acdfdcd6512a93c12ebd6f09684e4f0b9789d8
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-04 15:38:58 +0530

    work on admin home controller

--- controllers/admin/modules/home/controller.php
+++ controllers/admin/modules/home/controller.php
@@ -77,6 +77,7 @@ class homeController {
             foreach($aListEspData AS $aEspCOuntData)
             { 
                 $previousDate = date('Y-m-d',strtotime("-1 days"));
+                $previousDate = "2018-05-02";
                 $aEspDate = explode(' ',$aListEspData[$nCount]['esp_date']);
                 $previousDate .= " ".$aEspDate[1];
             
