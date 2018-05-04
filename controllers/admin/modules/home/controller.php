d6945c130e65e0adf6489b8497b039d643e1a33e
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-04 12:28:58 +0530

    work on admin home controller

--- controllers/admin/modules/home/controller.php
+++ controllers/admin/modules/home/controller.php
@@ -72,7 +72,7 @@ class homeController {
         $aEspDate = explode(' ',$aListEspData[0]['esp_date']);
         $previousDate .= " ".$aEspDate[1];
         var_dump($previousDate);
-        if (array_search($previousDate, $aListEspData[0])) {
+        if (array_search($previousDate, array_column($aListEspData,'esp_date'))) {
                echo "date in table"; exit;
         }
         if(empty($aListEspData) || $aEspDate[0] != $previousDate)
