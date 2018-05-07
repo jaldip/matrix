3b93fe35f484fa137d642b29edb1b2fe6215efcc
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-07 19:00:03 +0530

    work on graph module

--- controllers/admin/modules/home/dashboard.tpl.php
+++ controllers/admin/modules/home/dashboard.tpl.php
@@ -17,6 +17,9 @@
                     </div>
                 </li>
                 <ul class="nav side-menu">
+                    <li><a href="<?php echo getConfig('siteUrl') . '/home/dashboard' ?>"><i class="fa fa-home"><?php echo __('Home'); ?></i></a></li>
+                </ul>
+                <ul class="nav side-menu">
                     <li><a href="<?php echo getConfig('siteUrl') . '/graph/linegraph' ?>"><i class="fa fa-bar-chart-o"><?php echo __('Line Graph'); ?></i></a></li>
                 </ul>
             </ul>
