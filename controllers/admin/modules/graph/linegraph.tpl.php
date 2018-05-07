4b90e2763bab1b71a087657d479de9a6d559026e
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-07 19:42:50 +0530

    add moris js

--- controllers/admin/modules/graph/linegraph.tpl.php
+++ controllers/admin/modules/graph/linegraph.tpl.php
@@ -1,4 +1,71 @@
-      <!-- page content -->
+<div id="wrapper">
+    <nav class="navbar-default navbar-static-side" role="navigation">
+        <div class="sidebar-collapse">
+            <ul class="nav metismenu" id="side-menu">
+                <li class="nav-header">
+                    <div class="dropdown profile-element">
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <span class="clear"> 
+                                <span class="text-muted text-xs block"><?php echo __('profile'); ?><b class="caret"></b></span> </span> </a>
+                        <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                            <li><a href="<?php echo getConfig('siteUrl') . '/users/changepassword' ?>"><?php echo __('change_password'); ?></a></li>
+                            <li><a href="<?php echo getConfig('siteUrl') . '/users/logout' ?>"><?php echo __('logout'); ?></a></li>
+                        </ul>
+                    </div>
+                    <div class="logo-element">
+                        <?php echo __('MATRIX'); ?>
+                    </div>
+                </li>
+                <ul class="nav side-menu">
+                    <li><a href="<?php echo getConfig('siteUrl') . '/home/dashboard' ?>"><i class="fa fa-home"><?php echo __('Home'); ?></i></a></li>
+                </ul>
+                <ul class="nav side-menu">
+                    <li><a href="<?php echo getConfig('siteUrl') . '/graph/linegraph' ?>"><i class="fa fa-bar-chart-o"><?php echo __('Line Graph'); ?></i></a></li>
+                </ul>
+            </ul>
+
+        </div>
+    </nav>
+    <div id="page-wrapper" class="gray-bg">
+        <div class="row border-bottom">
+            <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+                <div class="navbar-header">
+                    <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+                </div>
+                <ul class="nav navbar-top-links navbar-right">
+                    <li>
+                        <span class="m-r-sm text-muted welcome-message"><?php echo __('welcome_to'); ?> <?php echo 'admin'; ?>.</span>
+                    </li>
+
+                    <li>
+                        <a href="<?php echo getConfig('siteUrl') . '/users/logout' ?>">
+                            <i class="fa fa-sign-out"></i> <?php echo __('logout'); ?>
+                        </a>
+                    </li>
+                </ul>
+
+            </nav>
+        </div>
+        <div class="row wrapper border-bottom white-bg page-heading">
+            <div class="col-lg-10">
+                <h2><?php echo __('MATRIX FILTER'); ?></h2>
+                <ol class="breadcrumb">
+                    <li>
+                        <a href="<?php echo getConfig('siteUrl') . '/home/dashboard'; ?>"><?php echo __('home'); ?></a>
+                    </li>
+                    <li>
+                        <a><?php echo __('MATRIX FILTER'); ?></a>
+                    </li>
+                    <li class="active">
+                        <strong><?php echo __('Data Tables'); ?></strong>
+                    </li>
+                </ol>
+            </div>
+            <div class="col-lg-2">
+
+            </div>
+        </div>
+    <!-- page content -->
       <div class="right_col" role="main">
         <div class="">
           <div class="page-title">
@@ -221,4 +288,3 @@
     <div class="clearfix"></div>
     <div id="notif-group" class="tabbed_notifications"></div>
   </div>
-
