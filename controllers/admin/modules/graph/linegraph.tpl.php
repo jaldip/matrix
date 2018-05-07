d709be4f32cb6cd5e92ef58979a0ebe8b2a2a829
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-07 19:45:45 +0530

    add moris js

--- controllers/admin/modules/graph/linegraph.tpl.php
+++ controllers/admin/modules/graph/linegraph.tpl.php
@@ -68,24 +68,7 @@
     <!-- page content -->
       <div class="right_col" role="main">
         <div class="">
-          <div class="page-title">
-            <div class="title_left">
-              <h3>Morris Js</h3>
-            </div>
-
-            <div class="title_right">
-              <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
-                <div class="input-group">
-                  <input type="text" class="form-control" placeholder="Search for...">
-                  <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">Go!</button>
-                        </span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="clearfix"></div>
-
+          
           <div class="row">
 
             <!-- bar chart -->
@@ -266,15 +249,11 @@
           </div>
         </div>
 
-        <!-- footer content -->
-        <footer>
-          <div class="copyright-info">
-            <p class="pull-right">Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
-            </p>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
+        <div class="footer">
+            <div>
+                <strong><?php echo __('for_any_query_please_contact_stepinsolutions'); ?></strong>
+            </div>
+        </div>
 
       </div>
       <!-- /page content -->
@@ -282,9 +261,4 @@
 
   </div>
 
-  <div id="custom_notifications" class="custom-notifications dsp_none">
-    <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
-    </ul>
-    <div class="clearfix"></div>
-    <div id="notif-group" class="tabbed_notifications"></div>
-  </div>
+  
