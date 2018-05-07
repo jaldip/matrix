0dc6b01f2156d227b7e4f6bb383143887cb582d0
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-07 20:40:09 +0530

    work on line graph

--- controllers/admin/templates/_footer.php
+++ controllers/admin/templates/_footer.php
@@ -1,26 +1,35 @@
+</div>
 
-    <!-- Mainly scripts -->
-    <!--<script src="<?php //echo getConfig('siteUrl') . '/js/jquery-3.1.1.min.js' ?>"></script>-->
-    <script src="<?php echo getConfig('siteUrl') . '/js/bootstrap.min.js' ?>"></script>
-    <script src="<?php echo getConfig('siteUrl') . '/js/plugins/metisMenu/jquery.metisMenu.js' ?>"></script>
-    <script src="<?php echo getConfig('siteUrl') . '/js/plugins/slimscroll/jquery.slimscroll.min.js' ?>"></script>
-    <script src="<?php echo getConfig('siteUrl') . '/js/plugins/dataTables/datatables.min.js' ?>"></script>
-    
-     <!--Custom and plugin javascript--> 
-    <script src="<?php echo getConfig('siteUrl') . '/js/inspinia.js' ?>"></script>
-    <script src="<?php echo getConfig('siteUrl') . '/js/plugins/pace/pace.min.js' ?>"></script>
-    <script src="<?php echo getConfig('siteUrl') . '/js/common.js' ?>"></script>
-     <!--daterangepicker--> 
-    <script src="<?php echo getConfig('siteUrl') .'/js/plugins/switchery/switchery.min.js'; ?>"></script>
-    <script src="<?php echo getConfig('siteUrl') .'/js/plugins/moment/moment.min.js'; ?>"></script>
-    <script src="<?php echo getConfig('siteUrl') .'/js/plugins/datepicker/daterangepicker.js'; ?>"></script>
-     <!-- color picker -->
-    <script src="<?php echo getConfig('siteUrl') .'/js/colorpicker/bootstrap-colorpicker.min.js';?>"></script>
-    <script src="<?php echo getConfig('siteUrl') .'/js/colorpicker/docs.js';?>"></script>
-    <!-- moris js -->
-    <script src="<?php echo getConfig('siteUrl') .'/js/moris/raphael-min.js'; ?>"></script>
-    <script src="<?php echo getConfig('siteUrl') .'/js/moris/morris.min.js';?>"></script>
-    <script src="<?php echo getConfig('siteUrl') .'/js/moris/example.js';?>"></script>
-    
-  
-          
+</div>
+
+<div id="custom_notifications" class="custom-notifications dsp_none">
+    <ul class="list-unstyled notifications clearfix" data-tabbed_notifications="notif-group">
+    </ul>
+    <div class="clearfix"></div>
+    <div id="notif-group" class="tabbed_notifications"></div>
+</div>
+<!-- Mainly scripts -->
+<!--<script src="<?php //echo getConfig('siteUrl') . '/js/jquery-3.1.1.min.js'   ?>"></script>-->
+<script src="<?php echo getConfig('siteUrl') . '/js/bootstrap.min.js' ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/plugins/metisMenu/jquery.metisMenu.js' ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/plugins/slimscroll/jquery.slimscroll.min.js' ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/plugins/dataTables/datatables.min.js' ?>"></script>
+
+<!--Custom and plugin javascript--> 
+<script src="<?php echo getConfig('siteUrl') . '/js/inspinia.js' ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/plugins/pace/pace.min.js' ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/common.js' ?>"></script>
+<!--daterangepicker--> 
+<script src="<?php echo getConfig('siteUrl') . '/js/plugins/switchery/switchery.min.js'; ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/plugins/moment/moment.min.js'; ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/plugins/datepicker/daterangepicker.js'; ?>"></script>
+<!-- color picker -->
+<script src="<?php echo getConfig('siteUrl') . '/js/colorpicker/bootstrap-colorpicker.min.js'; ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/colorpicker/docs.js'; ?>"></script>
+<!-- moris js -->
+<script src="<?php echo getConfig('siteUrl') . '/js/moris/raphael-min.js'; ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/moris/morris.min.js'; ?>"></script>
+<script src="<?php echo getConfig('siteUrl') . '/js/moris/example.js'; ?>"></script>
+
+
+
