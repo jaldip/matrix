5e7ab65422aeceb2b66d609dd315b70c12e3a04d
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-08 16:34:51 +0530

    addd report

--- controllers/admin/modules/report/controller.php
+++ controllers/admin/modules/report/controller.php
@@ -32,7 +32,7 @@ class reportController {
         $oEsp =new esp();
         $sListName = 'ALR';
         $aListEspData = $oEsp->getLastThirtyDaysRecords($sListName);
-        var_dump($aListEspData); EXIT;
+        
         $nCount = 0;
         foreach ($aListEspData as $aEspDetails)
         {    
