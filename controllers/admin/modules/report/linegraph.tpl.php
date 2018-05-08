55f4b6c4b30715265daa424d76608e9ac2283be0
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-08 16:26:29 +0530

    addd report

--- controllers/admin/modules/report/linegraph.tpl.php
+++ controllers/admin/modules/report/linegraph.tpl.php
@@ -223,14 +223,10 @@
     </div>
 
   </div>
-
+  <?php var_dump($aListData["payload"]); ?>
   <script>
       $(function () {
 
-    /* data stolen from http://howmanyleft.co.uk/vehicle/jaguar_'e'_type 
-     * 
-     */
-    <?php var_dump($aListData["payload"]); ?>
     Morris.Bar({
         element: 'graph_bar',
         data: [
