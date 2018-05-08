0a61bbe4476fae26ec27c15d1b0e4aa4e11f9e35
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-08 11:18:43 +0530

    work on graph module

--- web/admin/js/moris/example.js
+++ web/admin/js/moris/example.js
@@ -13,16 +13,16 @@ $(function () {
         {"period": "2016-09-15", "licensed": 2401, "sorned": 1656},
         {"period": "2016-09-10", "licensed": 2115, "sorned": 1022}
     ];
-    Morris.Bar({
-        element: 'graph_bar_group',
-        data: day_data,
-        xkey: 'period',
-        barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
-        ykeys: ['licensed', 'sorned'],
-        labels: ['Licensed', 'SORN'],
-        hideHover: 'auto',
-        xLabelAngle: 60
-    });
+//    Morris.Bar({
+//        element: 'graph_bar_group',
+//        data: day_data,
+//        xkey: 'period',
+//        barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
+//        ykeys: ['licensed', 'sorned'],
+//        labels: ['Licensed', 'SORN'],
+//        hideHover: 'auto',
+//        xLabelAngle: 60
+//    });
 
 //    Morris.Bar({
 //        element: 'graph_bar',
@@ -47,22 +47,22 @@ $(function () {
 //        hideHover: 'auto'
 //    });
 
-//    Morris.Bar({
-//        element: 'graphx',
-//        data: [
-//            {x: '2015 Q1', y: 2, z: 3, a: 4},
-//            {x: '2015 Q2', y: 3, z: 5, a: 6},
-//            {x: '2015 Q3', y: 4, z: 3, a: 2},
-//            {x: '2015 Q4', y: 2, z: 4, a: 5}
-//        ],
-//        xkey: 'x',
-//        ykeys: ['y', 'z', 'a'],
-//        barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
-//        hideHover: 'auto',
-//        labels: ['Y', 'Z', 'A']
-//    }).on('click', function (i, row) {
-//        console.log(i, row);
-//    });
+    Morris.Bar({
+        element: 'graphx',
+        data: [
+            {x: '2015 Q1', y: 2, z: 3, a: 4},
+            {x: '2015 Q2', y: 3, z: 5, a: 6},
+            {x: '2015 Q3', y: 4, z: 3, a: 2},
+            {x: '2015 Q4', y: 2, z: 4, a: 5}
+        ],
+        xkey: 'x',
+        ykeys: ['y', 'z', 'a'],
+        barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
+        hideHover: 'auto',
+        labels: ['Y', 'Z', 'A']
+    }).on('click', function (i, row) {
+        console.log(i, row);
+    });
 //
 //    Morris.Area({
 //        element: 'graph_area',
