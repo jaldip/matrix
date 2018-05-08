c1012e1adc9caddd903824c9d456924c09579ec2
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-08 11:23:34 +0530

    work on graph module

--- web/admin/js/moris/example.js
+++ web/admin/js/moris/example.js
@@ -13,39 +13,39 @@ $(function () {
         {"period": "2016-09-15", "licensed": 2401, "sorned": 1656},
         {"period": "2016-09-10", "licensed": 2115, "sorned": 1022}
     ];
-    Morris.Bar({
-        element: 'graph_bar_group',
-        data: day_data,
-        xkey: 'period',
-        barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
-        ykeys: ['licensed', 'sorned'],
-        labels: ['Licensed', 'SORN'],
-        hideHover: 'auto',
-        xLabelAngle: 60
-    });
-
 //    Morris.Bar({
-//        element: 'graph_bar',
-//        data: [
-//            {device: 'iPhone 4', geekbench: 380},
-//            {device: 'iPhone 4S', geekbench: 655},
-//            {device: 'iPhone 3GS', geekbench: 275},
-//            {device: 'iPhone 5', geekbench: 1571},
-//            {device: 'iPhone 5S', geekbench: 655},
-//            {device: 'iPhone 6', geekbench: 2154},
-//            {device: 'iPhone 6 Plus', geekbench: 1144},
-//            {device: 'iPhone 6S', geekbench: 2371},
-//            {device: 'iPhone 6S Plus', geekbench: 1471},
-//            {device: 'Other', geekbench: 1371}
-//        ],
-//        xkey: 'device',
-//        ykeys: ['geekbench'],
-//        labels: ['Geekbench'],
-//        barRatio: 0.4,
+//        element: 'graph_bar_group',
+//        data: day_data,
+//        xkey: 'period',
 //        barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
-//        xLabelAngle: 35,
-//        hideHover: 'auto'
+//        ykeys: ['licensed', 'sorned'],
+//        labels: ['Licensed', 'SORN'],
+//        hideHover: 'auto',
+//        xLabelAngle: 60
 //    });
+
+    Morris.Bar({
+        element: 'graph_bar',
+        data: [
+            {device: 'iPhone 4', geekbench: 380},
+            {device: 'iPhone 4S', geekbench: 655},
+            {device: 'iPhone 3GS', geekbench: 275},
+            {device: 'iPhone 5', geekbench: 1571},
+            {device: 'iPhone 5S', geekbench: 655},
+            {device: 'iPhone 6', geekbench: 2154},
+            {device: 'iPhone 6 Plus', geekbench: 1144},
+            {device: 'iPhone 6S', geekbench: 2371},
+            {device: 'iPhone 6S Plus', geekbench: 1471},
+            {device: 'Other', geekbench: 1371}
+        ],
+        xkey: 'device',
+        ykeys: ['geekbench'],
+        labels: ['Geekbench'],
+        barRatio: 0.4,
+        barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
+        xLabelAngle: 35,
+        hideHover: 'auto'
+    });
 
 //    Morris.Bar({
 //        element: 'graphx',
