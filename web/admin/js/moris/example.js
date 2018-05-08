8e99286cdb2c68f40c63fec21858210fa47636d1
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-08 11:17:03 +0530

    work on graph module

--- web/admin/js/moris/example.js
+++ web/admin/js/moris/example.js
@@ -24,28 +24,28 @@ $(function () {
 //        xLabelAngle: 60
 //    });
 
-    Morris.Bar({
-        element: 'graph_bar',
-        data: [
-            {device: 'iPhone 4', geekbench: 380},
-            {device: 'iPhone 4S', geekbench: 655},
-            {device: 'iPhone 3GS', geekbench: 275},
-            {device: 'iPhone 5', geekbench: 1571},
-            {device: 'iPhone 5S', geekbench: 655},
-            {device: 'iPhone 6', geekbench: 2154},
-            {device: 'iPhone 6 Plus', geekbench: 1144},
-            {device: 'iPhone 6S', geekbench: 2371},
-            {device: 'iPhone 6S Plus', geekbench: 1471},
-            {device: 'Other', geekbench: 1371}
-        ],
-        xkey: 'device',
-        ykeys: ['geekbench'],
-        labels: ['Geekbench'],
-        barRatio: 0.4,
-        barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
-        xLabelAngle: 35,
-        hideHover: 'auto'
-    });
+//    Morris.Bar({
+//        element: 'graph_bar',
+//        data: [
+//            {device: 'iPhone 4', geekbench: 380},
+//            {device: 'iPhone 4S', geekbench: 655},
+//            {device: 'iPhone 3GS', geekbench: 275},
+//            {device: 'iPhone 5', geekbench: 1571},
+//            {device: 'iPhone 5S', geekbench: 655},
+//            {device: 'iPhone 6', geekbench: 2154},
+//            {device: 'iPhone 6 Plus', geekbench: 1144},
+//            {device: 'iPhone 6S', geekbench: 2371},
+//            {device: 'iPhone 6S Plus', geekbench: 1471},
+//            {device: 'Other', geekbench: 1371}
+//        ],
+//        xkey: 'device',
+//        ykeys: ['geekbench'],
+//        labels: ['Geekbench'],
+//        barRatio: 0.4,
+//        barColors: ['#26B99A', '#34495E', '#ACADAC', '#3498DB'],
+//        xLabelAngle: 35,
+//        hideHover: 'auto'
+//    });
 
 //    Morris.Bar({
 //        element: 'graphx',
