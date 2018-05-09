6970ce417ebf225242c38ae557c880068491fcf3
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-09 15:12:46 +0530

    addd report

--- controllers/admin/modules/report/linegraph.tpl.php
+++ controllers/admin/modules/report/linegraph.tpl.php
@@ -28,16 +28,9 @@
             <div class="menu_section">
               <h3>General</h3>
               <ul class="nav side-menu">
-                <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                  <ul class="nav child_menu" style="display: none">
-                    <li><a href="index.html">Dashboard</a>
-                    </li>
-                    <li><a href="index2.html">Dashboard2</a>
-                    </li>
-                    <li><a href="index3.html">Dashboard3</a>
-                    </li>
-                  </ul>
+                <li><a href="<?php echo getConfig('siteUrl') . '/home/dashboard' ?>"><i class="fa fa-home"></i> Home </a>
                 </li>
+                <li><a href="<?php echo getConfig('siteUrl') . '/report/linegraph' ?>"><i class="fa fa-bar-chart-o" ><?php echo ' '.__('Report'); ?></i></a></li>
               </ul>  
         </div>
     </nav>
