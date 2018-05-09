9c1701ecdf046a40259184e042b3fee16826add3
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-09 16:10:49 +0530

    addd report

--- lib/esp.class.php
+++ lib/esp.class.php
@@ -162,13 +162,15 @@ class esp extends siCommon {
         $sQueryHendler = $this->getList($sSql, array(), array(), array(), array(), array());
         return $this->getData($sQueryHendler, "ARRAY");
     }
-    public function getLastThirtyDaysRecords($sListName='ALR') {
+    public function getLastThirtyDaysRecords($sListName='') {
         $sAndWhere = ' 1 = 1';
         $sAndWhere .= " AND e.deleted = 0 AND e.activated = 1 ";
         $sAndWhere .= " AND e.esp_date > CURDATE() - INTERVAL 31 DAY ";
         $sAndWhere .= " AND e.esp_date < CURDATE() ";
-       // $sAndWhere .= " AND e.esp_list_name = '$sListName'";
-        
+        if($sListName != '')
+        {    
+            $sAndWhere .= " AND e.esp_list_name = '$sListName'";
+        }
         $aGroupBy = array(' GROUP BY' => ' e.esp_date');
         
         $sSql = "SELECT 
