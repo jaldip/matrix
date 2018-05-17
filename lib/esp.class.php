b2a33d8523fcc02030de857c6fbd7be20f0d6cc6
shailesh.stepin <shaileshkhunt.stepin@gmail.com>
2018-05-17 12:19:03 +0530

    set cronjob in home module

--- lib/esp.class.php
+++ lib/esp.class.php
@@ -24,7 +24,7 @@ class esp extends siCommon {
      * @return boolean
      */
     public function addNewEsp($aEspData) {
-        $sTableName = 'esp';
+        $sTableName = 'esp_cron';
 
         $aInsertFieldArray = array(
                             'id_esp',
